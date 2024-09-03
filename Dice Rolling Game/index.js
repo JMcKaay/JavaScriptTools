7b7ed323030c47b7227f77a47f288139754fe48c
James <162724220+JMcKaay@users.noreply.github.com>
2024-09-03 19:54:02 +0100

    Updating images for dice game and header for main page

--- Dice Rolling Game/index.js
+++ Dice Rolling Game/index.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 
 
 function rollDice(){
@@ -17,23 +16,3 @@ function rollDice(){
     diceResult.textContent = `dice: ${values.join(', ')}`;
     diceImages.innerHTML = images.join('');
 }
-=======
-
-
-function rollDice(){
-    const numofDice = document.getElementById("numOfDice").value;
-    const diceResult = document.getElementById("diceResult");
-    const diceImages = document.getElementById("diceImages");
-    const values = [];
-    const images = [];
-
-    for(let i = 0; i < numofDice; i++){
-        const value = Math.floor(Math.random() * 6) + 1;
-        values.push(value);
-        images.push(`<img src="Dice_IMG/Dice${value}.png">`);
-    }
-
-    diceResult.textContent = `dice: ${values.join(', ')}`;
-    diceImages.innerHTML = images.join('');
-}
->>>>>>> 8aabc23 (Updating and mergin files. Removing Tet from outside <html> & footer text.)
--- Temperature Conversion Pro/index.js
+++ Temperature Conversion Pro/index.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 
 
 const textBox = document.getElementById("textBox");
@@ -28,35 +27,4 @@ function convert(){
 
 
 
-=======
-
-
-const textBox = document.getElementById("textBox");
-const toFahrenheit = document.getElementById("toFahrenheit");
-const toCelcius = document.getElementById("toCelcius");
-const result = document.getElementById("result");
-let temp; 
-
-
-
-
-function convert(){
-
-    if(toFahrenheit.checked){
-        temp = Number(textBox.value);
-        temp = temp  * 9 / 5 + 32;
-        result.textContent = temp.toFixed(1) + "°F";
-    } 
-    else if(toCelcius.checked) {
-        temp = Number(textBox.value);
-        temp = (temp - 32) * (5/9);
-        result.textContent = temp.toFixed(1) + "°C";
-    }
-    else {
-        result.textContent = "Select a unit";
-    }
-
-
-
->>>>>>> 8aabc23 (Updating and mergin files. Removing Tet from outside <html> & footer text.)
 }
